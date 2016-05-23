c52839cdc5d004d74390b68798aaf37172efc69f
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2016-05-23 21:51:49 +1000

    updated snp tables to align, now using click for interface

--- manuscript_scripts/snptables_to_aln.py
+++ manuscript_scripts/snptables_to_aln.py
@@ -3,8 +3,8 @@ from __future__ import division
 
 import os, sys, time
 from itertools import permutations
-from optparse import make_option
-from cogent.util.option_parsing import parse_command_line_parameters
+
+import click
 
 from mutation_motif.util import open_, create_path, abspath
 from scitrack import CachingLogger
@@ -164,62 +164,59 @@ def filtered_records(records, direction, seen, chroms, correct_chrom=everything,
         chroms.update([coord.split(':')[2]])
     
 
-def main(opts):
-    if not opts.dry_run:
-        create_path(opts.output_path)
+def run(input_path, output_path, direction, prefix, chrom_class, gc_class, freq_class, adjust_strand, limit, force_overwrite, dry_run, verbose):
+    if not dry_run:
+        create_path(output_path)
     
-    chrom_class = opts.chrom_class
-    freq_class = opts.freq_class
     correct_freq = {'All': everything,
         'Common': MakeFreqCompare(MAF, ge=True, get_freq=min,
-                                    verbose=opts.verbose),
+                                    verbose=verbose),
         'Rare': MakeFreqCompare(MAF, ge=False, get_freq=min,
-                                    verbose=opts.verbose)}[freq_class]
+                                    verbose=verbose)}[freq_class]
     
-    gc_class = opts.GC
     correct_comp = {'All': everything,
         'Hi': MakeFreqCompare(0.5, ge=True, get_freq=get_gc_freq,
-                                    verbose=opts.verbose),
+                                    verbose=verbose),
         'Lo': MakeFreqCompare(0.4, ge=False, get_freq=get_gc_freq,
-                                    verbose=opts.verbose)}[gc_class]
+                                    verbose=verbose)}[gc_class]
     
-    chrom_class = opts.chrom_class
     correct_chrom = {'All': everything, 'A': is_autosome,
                      'X': is_xchrom}[chrom_class]
     
-    direction = tuple(opts.direction.split('to'))
     seen = set()
     chroms = set()
-    if not os.path.exists(opts.input_path):
-        raise IOError("no files matching %s" % opts.input_path)
+    if not os.path.exists(input_path):
+        raise IOError("no files matching %s" % input_path)
     
     name_components = dict(freq_class='freq_'+freq_class,
             chrom_class='chrom_'+chrom_class,
-            gc_class='GC_'+gc_class, direction=opts.direction,
-            prefix = opts.prefix or '')
+            gc_class='GC_'+gc_class, direction=direction,
+            prefix = prefix or '')
     
-    outfilename = os.path.join(opts.output_path,
+    outfilename = os.path.join(output_path,
     '%(prefix)s%(freq_class)s-%(chrom_class)s-%(gc_class)s-%(direction)s.fasta.gz' % name_components)
     
-    runlog_path = os.path.join(opts.output_path,
+    runlog_path = os.path.join(output_path,
     '%(prefix)s%(freq_class)s-%(chrom_class)s-%(gc_class)s-%(direction)s.log' % name_components)
     LOGGER.log_file_path = runlog_path
     
-    if not opts.force_overwrite and (os.path.exists(outfilename) or os.path.exists(runlog_path)):
+    if not force_overwrite and (os.path.exists(outfilename) or os.path.exists(runlog_path)):
         msg = "Either %s or %s already exist. Force overwrite of existing files with -F."
         raise ValueError(msg % (outfilename, runlog_path))
  
-    LOGGER.input_file(opts.input_path)
+    LOGGER.input_file(input_path)
     
-    with open_(opts.input_path) as infile:
+    direction = tuple(direction.split('to'))
+    
+    with open_(input_path) as infile:
         with open_(outfilename, 'w') as outfile:
             num = 0
             for record in filtered_records(infile, direction, seen, chroms,
              correct_chrom=correct_chrom, correct_freq=correct_freq,
-             correct_comp=correct_comp, stranded=opts.adjust_strand, verbose=False):
+             correct_comp=correct_comp, stranded=adjust_strand, verbose=False):
                 outfile.write(record)
                 num += 1
-                if opts.limit and num >= opts.limit:
+                if limit and num >= limit:
                     break
         
         LOGGER.output_file(outfilename)
@@ -227,60 +224,45 @@ def main(opts):
     print msg
     LOGGER.log_message(msg + "\n", label="completed")
 
-script_info = {}
-script_info['brief_description'] = ""
-script_info['script_description'] = "export fasta formatted seqs matching specified conditions."
-
-
-script_info['required_options'] = [
-     make_option('-i','--input_path', help='glob pattern to data files.'),
-     make_option('-o','--output_path', help='Path to write data.'),
-     make_option('--direction', default=None,
-     choices=['AtoC', 'AtoG', 'AtoT', 'CtoA', 'CtoG', 'CtoT', 'GtoA', 'GtoC',
-             'GtoT', 'TtoA', 'TtoC', 'TtoG'], help='Mutation direction.'),
-    ]
-
-script_info['optional_options'] = [
-    make_option('-p','--prefix', help='Prefix for output file, e.g. intronic-'),
-    make_option('-c','--chrom_class', type='choice', default='All',
-        choices=['All', 'X', 'A', 'Y'], help='Chrom class [default:%default].'),
-    make_option('--GC', type='choice', default='All',
-        choices=['All', 'Hi', 'Lo'], help='GC proportion. Hi is >0.5, Lo is < 0.4.'),
-    make_option('--freq_class', type='choice', default='All',
-        choices=['All', 'Common', 'Rare'],
-        help='Frequency class. Common has MAF >0.05, rare <= 0.05.'),
-    make_option('-l','--limit', default=None, type=int,
-        help='Number of results to return.'),
-    make_option('-a','--adjust_strand', action='store_true', default=False,
-        help='Reverse complements records whose gene and snp strands differ.'),
-    make_option('-D','--dry_run', action='store_true', default=False,
-        help='Do a dry run of the analysis without writing output.'),
-    make_option('-F','--force_overwrite', action='store_true', default=False,
-        help='Overwrite output and run.log files.'),
-    make_option('-r','--reason', help='Reason for running analysis (for Sumatra log).'),
-    ]
-
-script_info['version'] = '0.1'
-script_info['authors'] = 'Gavin Huttley'
-
-if __name__ == "__main__":
-    option_parser, opts, args =\
-       parse_command_line_parameters(**script_info)
+@click.command()
+@click.option('-i','--input_path', required=True, help='glob pattern to data files.')
+@click.option('-o','--output_path', required=True, help='Path to write data.')
+@click.option('--direction', default=None, required=True,
+        type=click.Choice(['AtoC', 'AtoG', 'AtoT', 'CtoA', 'CtoG', 'CtoT',
+                'GtoA', 'GtoC', 'GtoT', 'TtoA', 'TtoC', 'TtoG']),
+                help='Mutation direction.')
+@click.option('-p','--prefix', help='Prefix for output file, e.g. intronic-')
+@click.option('-c','--chrom_class', default='All',
+        type=click.Choice(['All', 'X', 'A', 'Y']), help='Chrom class.')
+@click.option('--GC', default='All',
+        type=click.Choice(['All', 'Hi', 'Lo']),
+        help='GC proportion. Hi is >0.5, Lo is < 0.4.')
+@click.option('--freq_class', default='All',
+        type=click.Choice(['All', 'Common', 'Rare']),
+        help='Frequency class. Common has MAF >0.05, rare <= 0.05.')
+@click.option('-a','--adjust_strand', is_flag=True,
+        help='Reverse complements records whose gene and snp strands differ.')
+@click.option('-l','--limit', default=None, type=int,
+        help='Number of results to return.')
+@click.option('-F', '--force_overwrite', is_flag=True, help='Overwrite existing files.')
+@click.option('-D', '--dry_run', is_flag=True, help='Do a dry run of the analysis without writing output.')
+@click.option('--verbose', is_flag=True, help='Verbose output.')
+def main(input_path, output_path, direction, prefix, chrom_class, gc, freq_class, adjust_strand, limit, force_overwrite, dry_run, verbose):
+    """export fasta formatted seqs matching specified conditions."""
+    if not dry_run:
+        LOGGER.log_message("%s" % locals(), label="vars")
     
-    # determine the path to input data relative to sumatra's input data store
-    opts.input_path = abspath(opts.input_path)
-    opts.output_path = abspath(opts.output_path)
-    
-    if not opts.dry_run:
-        LOGGER.log_message("%s" % str(vars(opts)), label="vars")
-    
+    input_path = abspath(input_path)
+    output_path = abspath(output_path)
     
     start_time = time.time()
     
-    # run the program
-    main(opts)
+    run(input_path, output_path, direction, prefix, chrom_class, gc, freq_class, adjust_strand, limit, force_overwrite, dry_run, verbose)
     
     # determine runtime
     duration = time.time() - start_time
-    if not opts.dry_run:
+    if not dry_run:
         LOGGER.log_message("%.2f" % (duration/60.), label="run duration (minutes)")
+
+if __name__ == "__main__":
+    main()
